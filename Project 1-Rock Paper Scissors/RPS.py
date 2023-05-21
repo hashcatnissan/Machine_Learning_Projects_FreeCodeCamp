05a93df33c413f1962ec8d37d69d0763fc8b7ebe
hashcatnissan <s.m.arafatnishan2@gmail.com>
2023-05-21 22:43:11 +0600

    Rock Paper Scissors Project

---
+++ Project 1-Rock Paper Scissors/RPS.py
@@ -0,0 +1,26 @@
+def player(prev_play, opponent_history=[]):
+  if prev_play:
+    opponent_history.append(prev_play)
+  else:
+    opponent_history.clear()
+
+  counter = {'R': 'P', 'P': 'S', 'S': 'R'}
+  guess = "S"
+
+  if len(opponent_history) >= 4:
+    play_order = [
+      ''.join(opponent_history[k:k + 4])
+      for k, v in enumerate(opponent_history[:-3])
+    ]
+
+    potential_play = [
+      ''.join([*opponent_history[-3:], v]) for v in ['S', 'R', 'P']
+    ]
+
+    sub_order = {k: play_order.count(k) for k in potential_play}
+
+    prediction = max(sub_order, key=sub_order.get)[-1]
+
+    guess = counter[prediction]
+
+  return guess
